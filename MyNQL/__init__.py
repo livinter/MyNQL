84b25ceb2cb8ffc125c6f61e45202b7e2eb95c5c
Florian Scholz <livint@posteo.de>
2018-06-02 05:17:46 -0500

    url fix

--- MyNQL/__init__.py
+++ MyNQL/__init__.py
@@ -5,9 +5,9 @@ MyNQL is a Python package that work as a Network Database
 it should be a Network Language in the future, for now its only a library
 
 Source::
-    https://github.com/livinter/MyNLQ
+    https://github.com/livinter/MyNQL
 Bug reports::
-    https://github.com/livinter/MyNLQ/issues
+    https://github.com/livinter/MyNQL/issues
 
 """
 import logging
